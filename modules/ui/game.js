c83a27c55add5b3102cabdd4c4f8f7bdfb06570c
james-e-crandall <james.e.crandall@gmail.com>
2025-05-15 19:03:05 -0500

    visual remap

---
+++ modules/ui/game.js
@@ -0,0 +1,54 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_SUEBI': { //your unit type
+        unitType: 'UNIT_WARRIOR',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_SUEBI_2': { //your unit type
+        unitType: 'UNIT_SPEARMAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_SUEBI_3': { //your unit type
+        unitType: 'UNIT_PHALANX',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_FOLC_TEUTOBOD': { //your unit type
+        unitType: 'UNIT_LOGIOS_SOCRATES',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_FOLC_ARMINIUS': { //your unit type
+        unitType: 'UNIT_LOGIOS_XENOPHON',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },  
+    'UNIT_FOLC_VIBILIUS': { //your unit type
+        unitType: 'UNIT_LOGIOS_PYTHAGORAS',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_FOLC_CIVILIS': { //your unit type
+        unitType: 'UNIT_LOGIOS_PLATO',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },    
+    'UNIT_FOLC_VELEDA': { //your unit type
+        unitType: 'UNIT_TJATY_NEBET',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },      
+    'UNIT_FOLC_PIPARA': { //your unit type
+        unitType: 'UNIT_LOGIOS_HYPATIA',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    }, 
+    'UNIT_FOLC_HERMANAFRID': { //your unit type
+        unitType: 'UNIT_TJATY_IMHOTEP',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },   
+    'UNIT_FOLC_BOIORIX': { //your unit type
+        unitType: 'UNIT_TJATY_RAMOSE',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },  
+ 
+}
+
+
+
+
+
+
