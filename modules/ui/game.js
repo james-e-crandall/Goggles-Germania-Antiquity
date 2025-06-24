7647d6fd5b11da8fc05a9671510d84d7d192fc9a
james-e-crandall <james.e.crandall@gmail.com>
2025-06-24 18:42:22 -0500

    JEC

--- modules/ui/game.js
+++ modules/ui/game.js
@@ -1,46 +1,46 @@
 window.UnitPortraitVisualRemaps = {
     ...(window.UnitPortraitVisualRemaps || {}),
-    'UNIT_SUEBI': { //your unit type
+    'UNIT_SUEBI_JEC': { //your unit type
         unitType: 'UNIT_WARRIOR',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_SUEBI_2': { //your unit type
+    'UNIT_SUEBI_JEC_2': { //your unit type
         unitType: 'UNIT_SPEARMAN',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_SUEBI_3': { //your unit type
+    'UNIT_SUEBI_JEC_3': { //your unit type
         unitType: 'UNIT_PHALANX',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_FOLC_TEUTOBOD': { //your unit type
+    'UNIT_FOLC_JEC_TEUTOBOD': { //your unit type
         unitType: 'UNIT_LOGIOS_SOCRATES',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_FOLC_ARMINIUS': { //your unit type
+    'UNIT_FOLC_JEC_ARMINIUS': { //your unit type
         unitType: 'UNIT_LOGIOS_XENOPHON',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },  
-    'UNIT_FOLC_VIBILIUS': { //your unit type
+    'UNIT_FOLC_JEC_VIBILIUS': { //your unit type
         unitType: 'UNIT_LOGIOS_PYTHAGORAS',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
-    'UNIT_FOLC_CIVILIS': { //your unit type
+    'UNIT_FOLC_JEC_CIVILIS': { //your unit type
         unitType: 'UNIT_LOGIOS_PLATO',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },    
-    'UNIT_FOLC_VELEDA': { //your unit type
+    'UNIT_FOLC_JEC_VELEDA': { //your unit type
         unitType: 'UNIT_TJATY_NEBET',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },      
-    'UNIT_FOLC_PIPARA': { //your unit type
+    'UNIT_FOLC_JEC_PIPARA': { //your unit type
         unitType: 'UNIT_LOGIOS_HYPATIA',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     }, 
-    'UNIT_FOLC_HERMANAFRID': { //your unit type
+    'UNIT_FOLC_JEC_HERMANAFRID': { //your unit type
         unitType: 'UNIT_TJATY_IMHOTEP',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },   
-    'UNIT_FOLC_BOIORIX': { //your unit type
+    'UNIT_FOLC_JEC_BOIORIX': { //your unit type
         unitType: 'UNIT_TJATY_RAMOSE',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },  
