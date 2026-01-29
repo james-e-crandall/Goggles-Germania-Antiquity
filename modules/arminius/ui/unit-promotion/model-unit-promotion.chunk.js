881d42bf0012334e2a8f8f55df07b60f39c04525
james-e-crandall <james.e.crandall@gmail.com>
2026-01-29 09:22:57 -0600

    Modern Promotions

--- modules/arminius/ui/unit-promotion/model-unit-promotion.chunk.js
+++ modules/arminius/ui/unit-promotion/model-unit-promotion.chunk.js
@@ -2,6 +2,8 @@
 import { U as UnitPromotion } from '/base-standard/ui/unit-promotion/model-unit-promotion.chunk.js';
 
 UnitPromotion.iconClassMap.LOC_DISCIPLINE_ARMY_ARMINIUS_NAME = "promotion-assault";
+UnitPromotion.iconClassMap.LOC_DISCIPLINE_ARMY_OTTO_NAME = "promotion-bastion";
+UnitPromotion.iconClassMap.LOC_DISCIPLINE_SQUADRON_RICHTHOFEN_NAME = "promotion-dogfighting";
 
 engine.whenReady.then(() => {
   const updateModel = () => {
